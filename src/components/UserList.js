02b97e0d437c1c7d738c4ba9731934472617bbbf
Metin <metinferati15@gmail.com>
2022-03-16 11:12:39 +0100

    formatting update

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,60 +1,54 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
 function UserList() {
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [users, setusers] = useState([]);
 
-    const [error, setError] = useState(null);
-    const [isLoaded, setIsLoaded] = useState(false);
-    const [users, setusers] = useState([]);
-  
-    useEffect(() => {
-      fetch("https://gorest.co.in/public/v2/users")
-        .then(res => res.json())
-        .then(
-          (result) => {
-            setIsLoaded(true);
-            setusers(result);
-          },
-          (error) => {
-            setIsLoaded(true);
-            setError(error);
-          }
-        )
-    }, [])    
+  useEffect(() => {
+    setIsLoading(true);
+    fetch("https://gorest.co.in/public/v2/users")
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          setIsLoading(false);
+          setusers(result);
+        },
+        (error) => {
+          setIsLoading(false);
+          setError(error);
+        }
+      );
+  }, []);
 
-    users.sort((a, b) => a.id - b.id);
+  const sortedUserByStatus = users
+    .sort((a, b) => a.id - b.id)
+    .sort((a, b) => a.status.localeCompare(b.status));
 
-    users.sort((a, b) => a.status.localeCompare(b.status));
-
-    if (error) {
-        return <div>Error: {error.message}</div>;
-      } else if (!isLoaded) {
-        return <div>Loading...</div>;
-      } else {
-        return (
-            <div>
-               <table>
-               <thead>
-                      <tr>
-                          <th>ID</th>
-                          <th>
-                              Name
-                          </th>
-                          <th>Status</th>
-                      </tr>
-              </thead>
-              <tbody>
-              {users.map(user => (
-                      <tr key={user.id}>
-                          <td>{user.id}</td>
-                          <td>{user.name}</td>
-                          <td>{user.status}</td>
-                      </tr>
-                ))}
-              </tbody>
-              </table>
-            </div>
-          );
-      }
+  if (isLoading) return <div>Loading..</div>;
+  if (!isLoading && error) return <div>Error</div>;
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          {sortedUserByStatus.map((user) => (
+            <tr key={user.id}>
+              <td>{user.id}</td>
+              <td>{user.name}</td>
+              <td>{user.status}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default UserList;
